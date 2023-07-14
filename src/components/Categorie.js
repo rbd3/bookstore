8b470c61c06d686545aa2dbf58041f96b4863c96
rbd3 <rabedesanaa@gmail.com>
2023-07-14 09:48:38 +0300

    Setup project

---
+++ src/components/Categorie.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Categorie() {
+  return (
+    <div>In Construction</div>
+  )
+}
+
+export default Categorie
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => (
+  <header className="navbar">
+    <h1 className="header">Bookstore CMS</h1>
+    <ul className="links">
+      <li>
+        <NavLink to="/" className="link">
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/categories" className="link">
+          Categories
+        </NavLink>
+      </li>
+    </ul>
+  </header>
+);
+export default Navbar;
