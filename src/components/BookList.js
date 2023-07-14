122629aadb9e63d2499898466e4fc2c7d7fae86d
rbd3 <rabedesanaa@gmail.com>
2023-07-14 20:06:13 +0300

    Fix linters error

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -5,23 +5,23 @@ import Book from './Home';
 const BookList = ({ books }) => (
   <div>
     {books.map((book) => (
-    <Book key={book.id} title={book.title} author={book.author} />
+      <Book key={book.id} title={book.title} author={book.author} />
     ))}
   </div>
 );
 
 BookList.propTypes = {
-    books: PropTypes.arrayOf(
+  books: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number,
       title: PropTypes.string,
       author: PropTypes.string,
-     }),
-   ),
+    }),
+  ),
 };
 
 BookList.defaultProps = {
-    books: [],
-  };
+  books: [],
+};
 
 export default BookList;
