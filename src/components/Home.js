563c6259eb550ed588f51e184091e0831f48974f
rbd3 <rabedesanaa@gmail.com>
2023-07-14 18:19:20 +0300

    Fix linters error

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import Button from './Button';
 
 const Book = ({ title, author }) => (
-  <div className='author-wrapper'>
-    <h3 className='athor-name'>{title}</h3>
+  <div className="author-wrapper">
+    <h3 className="athor-name">{title}</h3>
     <p>
       {author}
     </p>
